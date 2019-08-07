14bb5b2ad10a8b46f91ce37a0406f2ea25a8a576
db21 <you@example.com>
2019-08-07 13:31:45 +0300

    123

--- build/js/main.js
+++ build/js/main.js
@@ -66,4 +66,17 @@ $(document).ready(function () {
 
     $(".wrapper .watch").fancybox();
 
+
+
+    $('header ul li a').on( 'click', function(){
+        var el = $(this);
+        var dest = el.attr('href'); // получаем направление
+        if(dest !== undefined && dest !== '') { // проверяем существование
+            $('html').animate({
+                scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+            }, 500 // скорость прокрутки
+            );
+        }
+        return false;
+    });
 });
