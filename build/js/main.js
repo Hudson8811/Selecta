fd8e621865ce904cb24178056784f84c0b026911
db21 <you@example.com>
2019-12-17 08:17:20 +0300

    123

--- build/js/main.js
+++ build/js/main.js
@@ -8487,6 +8487,19 @@ $(document).ready(function () {
         return false;
     });
 
+    $('.scroll-mouse').on('click', function () {
+        var el = $(this);
+        var dest = el.attr('href'); // получаем направление
+        if (dest !== undefined && dest !== '') { // проверяем существование
+            $('body').removeClass('blocked');
+            $('html').animate({
+                scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+            }, 500 // скорость прокрутки
+            );
+        }
+        return false;
+    });
+
 
 
     //init-descctop-slider
